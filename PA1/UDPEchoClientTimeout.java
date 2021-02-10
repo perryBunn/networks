fef7c8f27ef5ddcc976b95cf9df74e2ec4bb2483
perryBunn <eagle4ever20@gmail.com>
2021-02-09 20:07:50 -0600

    it didnt work :(

--- PA1/UDPEchoClientTimeout.java
+++ PA1/UDPEchoClientTimeout.java
@@ -5,6 +5,7 @@ public class UDPEchoClientTimeout {
 
     private static final int TIMEOUT = 3000;   // Resend timeout (milliseconds)
     private static final int MAXTRIES = 5;     // Maximum retransmissions
+    static int MAX_RUN = 1000;
 
     public static void main(String[] args) throws IOException {
 
@@ -28,34 +29,53 @@ public class UDPEchoClientTimeout {
 
         socket.setSoTimeout(TIMEOUT);  // Maximum receive blocking time (milliseconds)
 
-        DatagramPacket sendPacket = new DatagramPacket(bytesToSend,  // Sending packet
-                bytesToSend.length, serverAddress, servPort);
+        for (int i = 0; i < MAX_RUN; i++) {
+            DatagramPacket sendPacket = new DatagramPacket(bytesToSend,  // Sending packet
+                    bytesToSend.length, serverAddress, servPort);
+            DatagramPacket receivePacket =                              // Receiving packet
+                    new DatagramPacket(new byte[bytesToSend.length], bytesToSend.length);
 
-        DatagramPacket receivePacket =                              // Receiving packet
-                new DatagramPacket(new byte[bytesToSend.length], bytesToSend.length);
+            int tries = 0;      // Packets may be lost, so we have to keep trying
+            boolean receivedResponse = false;
+            do {
+                socket.send(sendPacket);          // Send the echo string
+                double endTime = 0;
+                double startTime = 0;
+                try {
+                    startTime = System.nanoTime();
+                    socket.receive(receivePacket);  // Attempt echo reply reception
 
-        int tries = 0;      // Packets may be lost, so we have to keep trying
-        boolean receivedResponse = false;
-        do {
-            socket.send(sendPacket);          // Send the echo string
-            try {
-                socket.receive(receivePacket);  // Attempt echo reply reception
+                    if (!receivePacket.getAddress().equals(serverAddress))  // Check source
+                        throw new IOException("Received packet from an unknown source");
 
-                if (!receivePacket.getAddress().equals(serverAddress))  // Check source
-                    throw new IOException("Received packet from an unknown source");
+                    receivedResponse = true;
+                    endTime = System.nanoTime();
+                } catch (InterruptedIOException e) {  // We did not get anything
+                    tries += 1;
+                    System.out.println("Timed out, " + (MAXTRIES - tries) + " more tries...");
+                }
+                totalTimeLong += endTime - startTime;
+                tripTime = ((endTime / 1000000) - (startTime / 1000000));
+                if (tripTime > maxTripTime) {
+                    maxTripTime = tripTime;
+                }
+                if ((tripTime < minTripTime) && (minTripTime != 0.0)) {
+                    minTripTime = tripTime;
+                }
+                totalTime += tripTime;
+            } while ((!receivedResponse) && (tries < MAXTRIES));
 
-                receivedResponse = true;
-            } catch (InterruptedIOException e) {  // We did not get anything
-                tries += 1;
-                System.out.println("Timed out, " + (MAXTRIES-tries) + " more tries...");
-            }
-        } while ((!receivedResponse) && (tries < MAXTRIES));
-
-        if (receivedResponse)
-            System.out.println("Received: " + new String(receivePacket.getData()));
-        else
-            System.out.println("No response -- giving up.");
-
+            if (receivedResponse)
+                System.out.println("Received: " + new String(receivePacket.getData()));
+            else
+                System.out.println("No response -- giving up.");
+        }
         socket.close();
+        System.out.println("----------------------------------------");
+        System.out.println("# of Runs: " + MAX_RUN);
+        System.out.println("Total Time: " + totalTime + "ms | " + totalTimeLong + "ns");
+        System.out.println("Max Trip Time: " + maxTripTime + "ms");
+        System.out.println("Min Trip Time: " + minTripTime + "ms");
+        System.out.println("Avg Time: " + totalTime/MAX_RUN + "ms");
     }
 }
